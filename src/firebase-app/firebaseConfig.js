9c0a331e0ba98b8f10c0145f0c1285974b67fda2
vinh <vinhphamdev0406@gmail.com>
2022-10-11 21:07:41 +0700

    update

--- src/firebase-app/firebaseConfig.js
+++ src/firebase-app/firebaseConfig.js
@@ -7,12 +7,12 @@ import {
 import { getFirestore } from "firebase/firestore";
 import "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyC_eyDGG3AHsjSwbJ2mKIGgcIA54vySAH8",
-  authDomain: "movie-a9cb0.firebaseapp.com",
-  projectId: "movie-a9cb0",
-  storageBucket: "movie-a9cb0.appspot.com",
-  messagingSenderId: "362064536571",
-  appId: "1:362064536571:web:2aa0a5c2ddd9e50a38a15e",
+  apiKey: "AIzaSyCCyttdbxI1ERkjo0Ipnh9nVgXP8j8IeoI",
+  authDomain: "movie-app-41ce9.firebaseapp.com",
+  projectId: "movie-app-41ce9",
+  storageBucket: "movie-app-41ce9.appspot.com",
+  messagingSenderId: "334263502846",
+  appId: "1:334263502846:web:e321fc97d01b3ff821a2af",
 };
 
 const app = initializeApp(firebaseConfig);
